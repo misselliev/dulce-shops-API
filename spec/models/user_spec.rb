1d12b378654242cc8e3c9a9a931a34c8763d47c0
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-06-17 16:10:23 -0700

    Updating tests for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,17 +3,35 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   let(:user) { FactoryBot.build(:user, :valid_user) }
   let(:invalid_user) { FactoryBot.build(:user, :invalid_user) }
+  let(:user2) { FactoryBot.build(:user, :valid_user) }
 
   before { user.save }
+  before { invalid_user.save }
+  before { user2.save }
 
   it 'should be valid' do
     expect(user).to be_valid
   end
 
-  before { invalid_user.save }
-
-  it 'password should not be too short' do
-    invalid_user.password = '1'
+  it 'password should not be less than 6 characters' do
     expect(invalid_user).to_not be_valid
+  end
+
+  it 'name and last name should be provided' do
+    expect(invalid_user).to_not be_valid
+    expect(user).to be_valid
+  end
+
+  it 'address should be provided' do
+    expect(invalid_user).to_not be_valid
+    expect(user).to be_valid
+  end
+
+  it 'has a unique username' do
+    expect(user2).to_not be_valid
+  end
+
+  it 'had a unique email' do
+    expect(user2).to_not be_valid
   end
 end
